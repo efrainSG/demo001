aeeea593b611f2ee0d92d8ee52cbe22c120b0b58
DESKTOP-8RJ1OMB\efrai <efrain_serna@hotmail.com>
2018-04-12 00:15:40 -0500

    Componente de contacto en JS con Ajax y JSON

--- sernasistemas/Controllers/HomeController.cs
+++ sernasistemas/Controllers/HomeController.cs
@@ -2,47 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using SernaSistemas.Core.Models;
 using SernaSistemas.Models;
 using WCFSernaSistemasLib;
 
-namespace SernaSistemas.Controllers {
-    public class HomeController : Controller {
+namespace SernaSistemas.Controllers
+{
+    public class HomeController : Controller
+    {
         const string URL_BLOGGER = "https://itcoffeecups.blogspot.com/";
 
-        private BloggerFeedModel GetPosts(string APIKey, string BlogID) {
+        private BloggerFeedModel GetPosts(string APIKey, string BlogID)
+        {
             BloggerFeedModel model = new BloggerFeedModel();
 
             Google.Apis.Blogger.v3.BloggerService servicio = new Google.Apis.Blogger.v3.BloggerService(
-                new Google.Apis.Services.BaseClientService.Initializer() {
+                new Google.Apis.Services.BaseClientService.Initializer()
+                {
                     ApiKey = APIKey
                 });
             var r = servicio.Posts.List(BlogID);
-            try {
+            try
+            {
                 r.MaxResults = 10;
                 var res = r.Execute();
-                var ft = (from i in res.Items select new {
-                    i.Title,
-                    i.Url,
-                    i.Content
-                }).ToList();
+                var ft = (from i in res.Items
+                          select new
+                          {
+                              i.Title,
+                              i.Url,
+                              i.Content
+                          }).ToList();
                 int k = 0;
-                foreach (var item in ft) {
+                foreach (var item in ft)
+                {
                     model.entries.Add(item.Title, item.Url);
-                    if (k == 0) {
+                    if (k == 0)
+                    {
                         var c = item.Content.Substring(item.Content.IndexOf("<section>", 0), 200) + "...";
                         c = c.Replace("<section>", "").Replace("</section>", "").Replace("<p>", "").Replace("</p>", "").Replace("<p class=\"comentario\">", "").Replace("<code>", "").Replace("<br />", "");
                         model.topEntryContent = new KeyValuePair<string, string>(item.Title, c);
                     }
                     k++;
                 }
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 model.MsgError = ex.Message;
             }
             return model;
         }
 
-        public ActionResult Index(ContactoModel model) {
+        public ActionResult Index(ContactoModel model)
+        {
             var data = ViewBag;
             data = ViewData;
             return View(GetPosts(
@@ -51,42 +64,68 @@ namespace SernaSistemas.Controllers {
                 ));
         }
 
-        public ActionResult About() {
+        public ActionResult About()
+        {
             return View();
         }
 
-        public ActionResult Contact() {
+        public ActionResult Contact()
+        {
             return View();
         }
 
-        public ActionResult Servicios() {
+        public ActionResult Servicios()
+        {
             return View();
         }
-        public ActionResult Productos() {
+        public ActionResult Productos()
+        {
             return View();
         }
 
-        public ActionResult FormContacto() {
+        public ActionResult FormContacto()
+        {
             return PartialView();
         }
 
         [HttpPost]
         [ActionName("FormContacto")]
-        public ActionResult PostContacto(ContactoModel model) {
+        public ActionResult PostContacto(ContactoModel model)
+        {
             model.Registrado = DateTime.Today;
             SernaSistemasServices servicio = new SernaSistemasServices();
             var response =
-            servicio.registrarContacto(new ContactoRequest() {
+            servicio.registrarContacto(new ContactoRequest()
+            {
                 Nombre = model.Nombre,
                 Telefono = model.Telefono,
-                 eMail = model.Email,
-                 Comentario = model.Comentario,
-                 FechaContacto = model.Registrado
+                eMail = model.Email,
+                Comentario = model.Comentario,
+                FechaContacto = model.Registrado
             });
             ViewData.Add("mensaje", response.Mensaje);
             if (response.tieneError)
                 return View("Error", response);
             return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        public JsonResult SendContacto(string nombre, string telefono, string correo, string comentario)
+        {
+            //model.Registrado = DateTime.Today;
+            SernaSistemasServices servicio = new SernaSistemasServices();
+            var response =
+            servicio.registrarContacto(new ContactoRequest()
+            {
+                Nombre = nombre,
+                Telefono = telefono,
+                eMail = correo,
+                Comentario = comentario,
+                FechaContacto = DateTime.Today
+            });
+            //ViewData.Add("mensaje", response.Mensaje);
+            if (response.tieneError)
+                return Json(new { error = "SI", msg = response.Mensaje });
+            return Json(new { error = "NO", msg = "Pronto me pondré en contacto contigo." });
         }
     }
 }
---
+++ sernasistemas/Scripts/Serna/contactoWidget.js
@@ -0,0 +1,136 @@
+﻿var initContacto = function (element) {
+    console.log(element);
+    var $form, $filaNombre, $filaTelefono, $filaCorreo, $filaComentario, $filaBoton;
+    var $txtNombre, $txtTelefono, $txtCorreo, $txtComentario, $btnEnviar;
+    var $addonNombre, $addonTelefono, $addonCorreo, $addonComentario;
+    var $msgNombre, $msgTelefono, $msgCorreo, $msgComentario;
+    var $celNombre, $celTelefono, $celCorreo, $celComentario, $celBoton;
+
+    $form = $('<form id="frmContacto" class="form-group well">')
+        .append('<h4 class="text-primary">Ponte en contacto conmigo</h4>')
+        .append('<span id="spanResultado" style="display:none;"></span>');
+    $txtNombre = $('<input type="text" id="txtNombre" class="form-control" name="txtNombre" placeholder="Escribe tu nombre" required>');
+    $txtTelefono = $('<input type="text" id="txtTelefono" class="form-control" name="txtTelefono" placeholder="Escribe tu teléfono o celular" required>');
+    $txtCorreo = $('<input type="mail" id="txtCorreo" class="form-control" name="txtCorreo" placeholder="Escribe tu dirección de correo" required>');
+    $txtComentario = $('<textarea id="txtComentario" class="form-control" name="txtComentario" placeholder="describe en qué te puedo ayudar" rows="3" required>');
+    $msgNombre = $('<span id="msgNombre" class="text-danger bg-danger" style="display:none;">Necesito saber tu nombre</span><br />');
+    $msgCorreo = $('<span id="msgCorreo" class="text-danger bg-danger" style="display:none;">Necesito tu correo para contactarte</span><br />');
+    $msgComentario = $('<span id="msgComentario" class="text-danger bg-danger" style="display:none;">Te faltó decirme en qué puedo ayudarte</span><br />');
+    $msgTelefono = $('<span id="msgTelefono" class="text-info bg-info" style="display:none;">Es recomendable que me des un número para localizarte</span><br />');
+    $btnEnviar = $('<a href="#" id="btnEnviarComentario" class="btn btn-lg btn-primary">Enviar</a>')
+        .off()
+        .on("click", enviarComentario_click);
+
+    $addonNombre = $('<span class="input-group-addon" id="addonNombre"><i class="glyphicon glyphicon-user"></i></span>');
+    $addonTelefono = $('<span class="input-group-addon" id="addonTelefono"><i class="glyphicon glyphicon-phone-alt"></i></span>');
+    $addonCorreo = $('<span class="input-group-addon" id="addonCorreo"><i class="glyphicon glyphicon-envelope"></i></span>');
+    $addonComentario = $('<span class="input-group-addon" id="addonComentario"><i class="glyphicon glyphicon-comment"></i></span>');
+
+    $celNombre = $('<div class="col-md-12 input-group">')
+        .append($addonNombre)
+        .append($txtNombre)
+        .append($msgNombre);
+    $celTelefono = $('<div class="col-md-12 input-group">')
+        .append($addonTelefono)
+        .append($txtTelefono)
+        .append($msgTelefono);
+    $celComentario = $('<div class="col-md-12 input-group">')
+        .append($addonComentario)
+        .append($txtComentario)
+        .append($msgComentario);
+    $celCorreo = $('<div class="col-md-12 input-group">')
+        .append($addonCorreo)
+        .append($txtCorreo)
+        .append($msgCorreo);
+    $celBoton = $('<div class="col-md-12">')
+        .append($btnEnviar);
+
+    $filaNombre = $('<div class="row">')
+        .append($celNombre);
+    $filaTelefono = $('<div class="row">')
+        .append($celTelefono);
+    $filaCorreo = $('<div class="row">')
+        .append($celCorreo);
+    $filaComentario = $('<div class="row">')
+        .append($celComentario);
+    $filaBoton = $('<div class="row">')
+        .append($celBoton);
+
+    $form
+        .append($filaNombre)
+        .append($filaTelefono)
+        .append($filaCorreo)
+        .append($filaComentario)
+        .append($filaBoton);
+    element
+        .append($form);
+}
+
+var enviarComentario_click = function () {
+    var $form = $("#frmContacto"),
+        errores = 0;
+    var $txtNombre = $("#txtNombre"),
+        $txtTelefono = $("#txtTelefono"),
+        $txtCorreo = $("#txtCorreo"),
+        $txtComentario = $("#txtComentario"),
+        $btnEnviar = $("#btnEnviar");
+    var $msgNombre = $("#msgNombre"),
+        $msgTelefono = $("#msgTelefono"),
+        $msgCorreo = $("#msgCorreo"),
+        $msgComentario = $("#msgComentario"),
+        $btnEnviar = $("#btnEnviarComentario");
+
+    if ($txtNombre.val().trim() == "") {
+        $msgNombre.show();
+        errores++;
+    }
+    if ($txtCorreo.val().trim() == "") {
+        $msgCorreo.show();
+        errores++;
+    }
+    if ($txtComentario.val().trim() == "") {
+        $msgComentario.show();
+        errores++;
+    }
+    if (errores != 0) {
+        if ($txtTelefono.val().trim() == "") {
+            $msgTelefono.show();
+        }
+        $('[id^="msg"]').delay(1000).fadeOut();
+    } else {
+        var clases = $btnEnviar.attr("class");
+        var url = window.location;
+        var server = url.protocol + '//' + url.host + (url.host == "localhost" ? url.port : "") + "/Home/SendContacto";
+        debugger;
+        $btnEnviar.addClass("disabled");
+        $.ajax({
+            url: server,
+            data: {
+                nombre: $txtNombre.val(),
+                telefono: $txtTelefono.val(),
+                correo: $txtCorreo.val(),
+                comentario: $txtComentario.val()
+            },
+            type: "post",
+            dataType: "json"
+        })
+            .done(function (obj) {
+
+                $("#spanResultado")
+                    .empty()
+                    .addClass(
+                    (obj.error == "NO") ? "text-info" : "text-error"
+                    )
+                    .append(obj.msg)
+                    .fadeIn()
+                    .delay(3500)
+                    .fadeOut("slow");
+            })
+            .fail(function (a) {
+                console.log(a);
+            })
+            .always(function (e) {
+                $btnEnviar.removeClass("disabled");
+            });
+    }
+}
