86ced467cedcf8b21e3ba12bffa525333120f10a
DESKTOP-8RJ1OMB\efrain <efrain_serna@hotmail.com>
2018-04-27 22:58:09 -0500

    Agregado de pseudo-widgets iniciales para Biblioteca

---
+++ sernasistemas/Scripts/Biblioteca/BuscarWidget.js
@@ -0,0 +1,11 @@
+﻿/*
+ * Hace la búsqueda de libros en las bibliotecas registradas
+ */
+var initBuscador = function (element) {
+    var $form, $filaBuscar, $celBuscar, $txtBuscar, $addonBuscar, $btnBuscar;
+
+}
+
+var buscar_onclick = function () {
+    var $form, $txtBuscar, $btnBuscar;
+}
